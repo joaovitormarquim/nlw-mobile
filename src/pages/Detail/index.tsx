3277497804966e17b480b76b42112c90efdd32c4
João Vítor Marquim <jvmsmfn@cin.ufpe.br>
2020-06-04 23:51:09 -0300

    Add link to whatsapp and mail composer

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import Constants from 'expo-constants';
-import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler'
+import { View, StyleSheet, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
+import * as MailComposer from 'expo-mail-composer';
+
+interface Params {
+  point_id: number;
+}
+
+interface Data {
+  point:{
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
+  items: {
+    title: string
+  }[];
+}
 
 const Detail = () => {
+  const [data, setData] = useState<Data>({} as Data);
+
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`)
+      .then(response => {
+        setData(response.data);
+      });
+  }, []);
 
   function handleNavigateBack() {
     navigation.goBack();
+  }
+
+  function handleComposeMail() {
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos',
+      recipients: [data.point.email],
+
+    });
+  }
+
+  function handleWhatsapp() {
+    console.log(data.point.whatsapp)
+    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos`);
+  }
+
+  if (!data.point) {
+    return null;
   }
 
   return (
@@ -21,21 +70,23 @@ const Detail = () => {
 
         <Image style={styles.pointImage} source={{ uri: 'https://www.infovarejo.com.br/novo/wp-content/uploads/2017/08/O-que-fazer-com-a-capacidade-ociosa-do-seu-supermercado-740.jpg' }} />
 
-        <Text style={styles.pointName}>Mercadão</Text>
-        <Text style={styles.pointItems}>Oleo, Pilha</Text>
+        <Text style={styles.pointName}>{data.point.name}</Text>
+        <Text style={styles.pointItems}>
+          {data.items.map(item => item.title).join(', ')}
+        </Text>
 
         <View style={styles.address}>
           <Text style={styles.addressTitle}>Endereço</Text>
-          <Text style={styles.addressContent}>Serra Talhada, PE</Text>
+          <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
         </View>
       </View>
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleWhatsapp}>
           <FontAwesome name="whatsapp" size={20} color="#FFF" />
           <Text style={styles.buttonText}>Whatsapp</Text>
         </RectButton>
 
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleComposeMail}>
           <Icon name="mail" size={20} color="#FFF" />
           <Text style={styles.buttonText}>E-mail</Text>
         </RectButton>
